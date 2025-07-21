a82dd676b0eda9e9bce37c612ee7fa3cc8fe4b92
dmacedo <devinm3333@outlook.com>
2025-07-21 11:52:12 -0700

    Lecture Complete

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const localPort = 3000;
 const express = require('express');
 const dotenv = require('dotenv');
 const Fruit = require('./models/fruit.js');
+const methodOverride = require("method-override"); // new
+const morgan = require("morgan"); //new
 
+dotenv.config();
 const app = express();
 app.use(express.urlencoded({ extended: false }));
-dotenv.config();
+app.use(methodOverride("_method")); // new
+app.use(morgan("dev")); //new
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI);
@@ -36,7 +40,7 @@ app.get('/fruits/new', (req, res) => {
 
 app.get('/fruits', async (req,res) => {
   const allFruits = await Fruit.find({});
-  console.log(allFruits);
+  // console.log(allFruits);
 
   // let sendHtml = '<h1>All Fruits</h1><ul>';
   // sendHtml += allFruits.reduce((accu, fruit) => {
@@ -54,18 +58,36 @@ app.get('/fruits', async (req,res) => {
 app.post('/fruits', async (req, res) => {
   req.body.isReadyToEat = (req.body.isReadyToEat === 'on');
   req.body.name = req.body.name.trim();
-  console.log('New Fruit:', req.body);
+  // console.log('New Fruit:', req.body);
 
   if (req.body.name) await Fruit.create(req.body);
   res.redirect('/fruits');
 });
 
-app.delete('/fruits', async (req, res) => {
-  req.body.isReadyToEat = (req.body.isReadyToEat === 'on');
-  req.body.name = req.body.name.trim();
+app.get('/fruits/:id', async (req,res) => {
+  const fruit = await Fruit.findById(req.params.id);
+  // console.log(fruit).name;
+  res.render("fruits/show.ejs", { fruit: fruit });
+  // res.send(fruit);
+});
 
-  // await Fruit.deleteOne(req.body);
-  res.redirect('/fruit/new');
+app.delete('/fruits/:id', async (req, res) => {
+  // res.send(req.params.id);
+  await Fruit.findByIdAndDelete(req.params.id);
+  res.redirect('/fruits');
+});
+
+app.get('/fruits/:id/edit', async (req,res) => {
+  const fruit = await Fruit.findById(req.params.id);
+  console.log(fruit);
+  // res.send(`This is the edit route for ${fruit.name}`);
+  res.render('fruits/edit.ejs', {fruit: fruit});
+});
+
+app.put('/fruits/:id', async (req,res) => {
+  req.body.isReadyToEat = (req.body.isReadyToEat === 'on');
+  await Fruit.findByIdAndUpdate(req.params.id, req.body);
+  res.redirect(`/fruits/${req.params.id}`);
 });
 
 app.listen(localPort, () => {
